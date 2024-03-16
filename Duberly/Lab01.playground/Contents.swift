f9e75ddd9501e98adef09a72792989995ae7ee9d
DuberlyIvanMondragonManchay <114697210+DuberlyIvanMondragonManchay@users.noreply.github.com>
2024-03-16 12:45:58 -0500

    Add files via upload

---
+++ Duberly/Lab01.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+/*
+ 
+ let para variables inmutables (Su valor no cambia)
+ -dni
+ -igv
+ 
+ var mutables (Su valor cambia)
+ -edad
+ -ventas
+ -precio
+ 
+ */
+var greeting = "Hello, playground"
